4a89d8c6fa5f3797ab2084c4f6d815f2d78e95b5
Tanner Woodrum <tanner_woodrum@techtanner.net>
2025-02-21 13:24:22 -0600

    adding your_exercises

---
+++ your_exercises/1_variables_and_general.py
@@ -0,0 +1,13 @@
+# function: Leap year checker
+    # receive a year from the user and convert it to an int
+    # Leap year logic: the year is a leap year if it is divisible by 4 AND not divisible by 100 while also being divisible by 400
+        # You'll need to remember that the % operator shows the remainder of a division operation
+            # ex: 4 % 2 = 0, 5 % 2 = 1
+        # the comparison operator for saying "not equal to" is !=
+            # ex:
+                # if "test" != "Test String":
+                    # print("run this code")
+
+
+
+
